08b195b052445d73220c435e3f448c676a402043
marufsiyam91 <marufsiyam9123@gmail.com>
2024-01-05 00:24:43 +0600

    route chage

--- src/main.jsx
+++ src/main.jsx
@@ -3,26 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
-import routes from "./Routes/routes.jsx";
+import routes from "./Routes/Routes.jsx";
 import { ToastContainer } from "react-toastify";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={routes}>
       <App />
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-      />
-      {/* Same as */}
       <ToastContainer />
     </RouterProvider>
   </React.StrictMode>
